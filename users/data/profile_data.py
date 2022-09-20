9682cb618bea05a4d245744a58466412bdc8952b
David <usmagrafico@gmail.com>
2022-09-20 17:05:07 -0500

    Fix bug with profile graph when there is not info

--- users/data/profile_data.py
+++ users/data/profile_data.py
@@ -15,9 +15,6 @@ def profile_data(context):
 
     d = {
         'Peso': [profile.weight for profile in profiles],
-        # 'Velocidad 1': [profile.speed1 for profile in profiles],
-        # 'Velocidad 2': [profile.speed2 for profile in profiles],
-        # 'Velocidad 3': [profile.speed3 for profile in profiles],
     }
 
     for i in range(1,5):
@@ -43,6 +40,7 @@ def profile_data(context):
             'Peso': [0, 0],
             'Velocidad 1': [0, 0],
         }
+        max_speeds = [0, 0]
 
     df = pd.DataFrame(data=d)
     df.set_index('Peso', inplace=True)
@@ -50,20 +48,25 @@ def profile_data(context):
     df2 = pd.DataFrame(data=d)
     df2.set_index('Velocidad 1', inplace=True)
 
-    #Bar chart
-    #bi_fig = px.bar(df, labels={'weight':'Peso', 'value': 'Vel'}, barmode = 'group')
-    bi_fig = px.scatter(df, title='Perfil F/V', labels={'Peso':'PESO(KG)', 'value': 'VEL(m/s'}, trendline="ols", trendline_scope="overall")
-    bi_fig2 = px.scatter(x=max_speeds, y=d['Peso'], title='Perfil F/V', labels={'value':'PESO(KG)', 'Velocidad 1': 'VEL(m/s)'}, trendline="ols", trendline_scope="overall")
-    
-    model = px.get_trendline_results(bi_fig2)
-    results = model.iloc[0]["px_fit_results"]
+    if len(profiles) > 1:
+        #Bar chart
+        #bi_fig = px.bar(df, labels={'weight':'Peso', 'value': 'Vel'}, barmode = 'group')
+        bi_fig = px.scatter(df, title='Perfil F/V', labels={'Peso':'PESO(KG)', 'value': 'VEL(m/s'}, trendline="ols", trendline_scope="overall")
+        bi_fig2 = px.scatter(x=max_speeds, y=d['Peso'], title='Perfil F/V', labels={'value':'PESO(KG)', 'Velocidad 1': 'VEL(m/s)'}, trendline="ols", trendline_scope="overall")
+        
+        model = px.get_trendline_results(bi_fig2)
+        results = model.iloc[0]["px_fit_results"]
 
-    print(results.params)
-    
-    rm = round(results.params[1]*0.3+results.params[0], 2)
+        #print(results.params)
+        
+        rm = round(results.params[1]*0.3+results.params[0], 2)
 
+        context['pgraph2'] = bi_fig2.to_html
+        context['rm'] = rm
+    else:
+        bi_fig = px.scatter(x=[0,], y=[0,], title='Perfil F/V', labels={'Peso':'PESO(KG)', 'value': 'VEL(m/s'}, trendline="ols", trendline_scope="overall")
+
+    context['pgraph'] = bi_fig.to_html
     context['profiles'] = profiles
     context['df'] = df2.to_html
-    context['pgraph'] = bi_fig.to_html
-    context['pgraph2'] = bi_fig2.to_html
-    context['rm'] = rm
+    
