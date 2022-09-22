9e949e5d7948d8d68da748225ac1cf7a132e843b
David <usmagrafico@gmail.com>
2022-09-22 12:39:45 -0500

    Change settings

---
+++ metrics/settings/prod.py
@@ -0,0 +1,15 @@
+from .base import *
+
+# Database
+# https://docs.djangoproject.com/en/4.1/ref/settings/#databases
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': os.getenv('MYSQL_DATABASE'),
+        'USER': os.getenv('MYSQL_USER'),
+        'PASSWORD': os.getenv('MYSQL_PASSWORD'),
+        'HOST': os.getenv('MYSQL_HOST'),
+        'PORT': os.getenv('MYSQL_PORT', 3306)
+    }
+}
