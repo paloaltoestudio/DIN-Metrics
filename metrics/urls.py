61c5a7c2af35ecfd405a5b6ff82cb7c80a19480a
David <usmagrafico@gmail.com>
2022-09-21 19:07:15 -0500

    Update url to make root sportmen list

--- metrics/urls.py
+++ metrics/urls.py
@@ -4,8 +4,8 @@ from django.urls import path, include
 from users.views import UserLoginView, logout_view
 
 urlpatterns = [
+    path('', include('users.urls')),
     path('admin/', admin.site.urls),
     path('login', UserLoginView.as_view(), name='login'),
     path('logout', logout_view, name='logout'),
-    path('usuarios/', include('users.urls')),
 ]
