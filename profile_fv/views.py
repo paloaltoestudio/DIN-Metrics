cdb2442dd509997f2d003bcae4231323d8524b82
David <usmagrafico@gmail.com>
2022-09-23 09:06:38 -0500

    Add create metric option to profile F/V page

--- profile_fv/views.py
+++ profile_fv/views.py
@@ -4,14 +4,20 @@ from django.urls import reverse_lazy
 #Models
 from .models import Profile_fv
 
-class ProfileUpdateView(UpdateView):
+class ProfileBase:
+    def get_success_url(self):
+        id = self.request.POST['uid']
+        success_url = reverse_lazy('users:profile_fv_detail', kwargs = {'id': id})
+        url = success_url.format(**self.object.__dict__)
+        return url
+
+
+class ProfileUpdateView(ProfileBase, UpdateView):
     model = Profile_fv
     fields = ['weight', 'speed1', 'speed2', 'speed3', 'speed4']
     pk_url_kwarg = 'id'
 
-    def get_success_url(self):
-        id = self.request.POST['uid']
-        success_url = reverse_lazy('users:user_detail', kwargs = {'id': id})
-        url = success_url.format(**self.object.__dict__)
-        url = url + '?tab=profile'
-        return url
+
+class ProfileCreateView(ProfileBase, CreateView):
+    model = Profile_fv
+    fields = ['athlete', 'weight', 'speed1', 'speed2', 'speed3', 'speed4']
