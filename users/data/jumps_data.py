fb846138b6fc8a0bc73bfb1029a68320ff2a39a6
David <usmagrafico@gmail.com>
2022-10-27 13:27:45 -0500

    Updates in neuro muscular tab

--- users/data/jumps_data.py
+++ users/data/jumps_data.py
@@ -12,71 +12,51 @@ from neuro.models import SJ, CMJ, DROPS, Q
 from .data_utils import update_plot
 
 def jump_data(context):
-
-    x = ['Mes ' + str(x) for x in range(1, 13)]
-  
-    fig2 = go.Figure(data=[go.Bar(
-        name = 'SJ',
-        x = x,
-        y = [100, 200, 500, 673, 600, 500, 502, 405, 609, 700, 750, 760]
-    ),
-        go.Bar(
-        name = 'CMJ',
-        x = x,
-        y = [56, 123, 982, 213, 500, 673, 600, 500, 502, 405, 300, 230]
-    )
-    ])
-
-    jumps = {
-        'SJ': {
-            'm1': 56,
-            'm2': 123,
-        },
-        'CMJ': {
-            'm1': 34,
-            'm2': 58,
-        },
-    }
     
-    
-    sj = SJ.objects.filter(athlete=context['user'].athlete)
+    sj = SJ.objects.filter(athlete=context['user'].athlete).order_by('date')
     cmj = CMJ.objects.filter(athlete=context['user'].athlete)
     drops = DROPS.objects.filter(athlete=context['user'].athlete)
     q = Q.objects.filter(athlete=context['user'].athlete)
 
-    d = {'SJ': [0,0,0,0,0,0,0,0,0,0,0,0], 
-         'CMJ': [0,0,0,0,0,0,0,0,0,0,0,0], 
-         'DROPS': [0,0,0,0,0,0,0,0,0,0,0,0], 
-         'Q': [0,0,0,0,0,0,0,0,0,0,0,0]}
+    jumps = []
+
+    d = {
+        'date':[],
+        'jump':[],
+        'score':[]
+    }
+
+    def add_jump(name, jump):
+        for item in jump:
+            jumps.append(item)
+
+            d['jump'].append(name)
+            d['date'].append(item.date) 
+            d['score'].append(item.score)
 
     if len(sj) > 0:
-        sj = sj[0]
-        d['SJ'] = [sj.month1, sj.month2, sj.month3, sj.month4, sj.month5, sj.month6,
-                    sj.month7, sj.month8, sj.month9, sj.month10, sj.month11, sj.month12]
+        add_jump('SJ', sj)
 
-    if len(cmj) > 0:             
-        cmj = cmj[0]
-        d['CMJ'] = [cmj.month1, cmj.month2, cmj.month3, cmj.month4, cmj.month5, cmj.month6,
-                    cmj.month7, cmj.month8, cmj.month9, cmj.month10, cmj.month11, cmj.month12]
+    if len(cmj) > 0:  
+        add_jump('CMJ', cmj)
 
     if len(drops) > 0:  
-        drops = drops[0]
-        d['DROPS'] = [drops.month1, drops.month2, drops.month3, drops.month4, drops.month5, drops.month6,
-                    drops.month7, drops.month8, drops.month9, drops.month10, drops.month11, drops.month12]
+        add_jump('DROPS', drops)
 
     if len(q) > 0:  
-        q = q[0]
-        d['Q'] = [q.month1, q.month2, q.month3, q.month4, q.month5, q.month6,
-                    q.month7, q.month8, q.month9, q.month10, q.month11, q.month12]
+        add_jump('Q', q)
 
-    df = pd.DataFrame(data=d, index=['Mes ' + str(x) for x in range(1, 13)])
+
+    #df = pd.DataFrame(data=d, index=['Mes ' + str(x) for x in range(1, 3)])
+    df = pd.DataFrame(data=d, index=d['date'])
 
     #Bar chart
-    #fig = px.bar(df, labels={'index':'Meses', 'value': 'Salto en CM'}, barmode = 'group')
-
-    #Line chart
-    fig = px.line(df, labels={'index':'Meses', 'value': 'Salto en CM'}, markers=True)
+    fig = px.bar(df, x= 'date', y = 'score', labels={'date':'Fecha', 'score': 'Salto en CM', 'color': 'Salto'}, color = d['jump'], barmode = 'group')
     update_plot(fig)
 
+    #Line chart
+    #fig = px.line(df, labels={'index':'Meses', 'value': 'Salto en CM'}, markers=True)
+
     context['fig'] = fig.to_html(include_plotlyjs="cdn", full_html=False)
-    context['jumps'] = d
+    context['jumps'] = jumps
+    #context['df'] = df.to_html(justify='left')
