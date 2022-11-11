ba83fb65eb1e71ead85c9a78c28f07cbd7eb10ed
David <usmagrafico@gmail.com>
2022-11-11 14:19:42 -0500

    Add bar plot to historic RM registers

--- users/data/profile_data.py
+++ users/data/profile_data.py
@@ -14,52 +14,24 @@ from .data_utils import update_plot
 
 def profile_data(context):
     
-    profiles = FV.objects.filter(athlete=context['user'].athlete)
+    profiles = FV.objects.filter(athlete=context['user'].athlete).order_by('date')
 
-    # is_limit = len(profiles) >= 5
+    d = {
+        'Fecha':[],
+        'RM':[],
+    }
 
-    # d = {
-    #     'Peso': [profile.weight for profile in profiles],
-    # }
+    if len(profiles) > 1:
+        for profile in profiles:
+            d['Fecha'].append(profile.date)
+            d['RM'].append(profile.rm)
 
-    # for i in range(1,5):
-    #     current_speed = 'speed'+str(i)
-    #     speed = [getattr(profile, current_speed) for profile in profiles]
-
-    #     if any(type(value) == float for value in speed if value is not None):
-    #         d['Velocidad '+str(i)] = speed
-
-    # #Get the max speed per row
-    # max_speeds = []
-    # for p in profiles:
-    #     max_speed = [p.speed1 if p.speed1 is not None else 0, 
-    #                  p.speed2 if p.speed2 is not None else 0,
-    #                  p.speed3 if p.speed3 is not None else 0,
-    #                  p.speed4 if p.speed4 is not None else 0,]
-
-    #     max_speeds.append(max(max_speed))
-
-
-    # if len(profiles) > 1:
-    #     df = pd.DataFrame(data=d)
-    #     df.set_index('Peso', inplace=True)
-
-    #     df2 = pd.DataFrame(data=d)
-    #     df2.set_index('Velocidad 1', inplace=True)
-
-    #     bi_fig = px.scatter(df, title='Perfil F/V', labels={'Peso':'PESO(KG)', 'value': 'VEL(m/s'}, trendline="ols", trendline_scope="overall")
-    #     update_plot(bi_fig)
-        
-    #     bi_fig2 = px.scatter(x=max_speeds, y=d['Peso'], title='Perfil F/V', labels={'value':'PESO(KG)', 'Velocidad 1': 'VEL(m/s)'}, trendline="ols", trendline_scope="overall")
-    #     update_plot(bi_fig2)
-
-    #     model = px.get_trendline_results(bi_fig2)
-    #     results = model.iloc[0]["px_fit_results"]
-        
-    #     rm = round(results.params[1]*0.3+results.params[0], 2)
-
-    #     context['graph'] = bi_fig.to_html(include_plotlyjs="cdn", full_html=False)
-    #     context['rm'] = rm
+        df = pd.DataFrame(data=d)
+        df.set_index('Fecha', inplace=True)
+        print(df)
+        bi_fig = px.bar(df, title='Perfil F/V', color=d['RM'], color_continuous_scale=[(0, "red"), (0.4, "yellow"), (0.6, "orange"), (1, "blue")], labels={'value': 'RM'})
+        update_plot(bi_fig)
+        context['graph'] = bi_fig.to_html(include_plotlyjs="cdn", full_html=False)
         
     context['profiles'] = profiles
     # context['is_limit'] = is_limit
