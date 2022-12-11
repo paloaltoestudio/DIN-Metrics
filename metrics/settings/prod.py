6d1779dd0215a2f5a2ae64631fdf3d26f1e38276
David <david@paloaltoestudio.com>
2022-12-11 15:55:38 -0500

    Changes in docker compose fo rproduction

--- metrics/settings/prod.py
+++ metrics/settings/prod.py
@@ -3,14 +3,25 @@ from .base import *
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.mysql',
+#         'NAME': os.environ.get('MYSQL_DATABASE'),
+#         'USER': os.environ.get('MYSQL_USER'),
+#         'PASSWORD': os.environ.get('MYSQL_PASSWORD'),
+#         'HOST': os.environ.get('MYSQL_DATABASE_HOST'),
+#         'PORT': os.environ.get('MYSQL_DATABASE_PORT', 3306),
+#     }
+# }
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
-        'NAME': os.environ.get('MYSQL_DATABASE'),
-        'USER': os.environ.get('MYSQL_USER'),
-        'PASSWORD': os.environ.get('MYSQL_PASSWORD'),
-        'HOST': os.environ.get('MYSQL_DATABASE_HOST'),
-        'PORT': os.environ.get('MYSQL_DATABASE_PORT', 3306),
+        'NAME': 'dinametrics',
+        'USER': 'dina_user',
+        'PASSWORD': 'dinametrics@2022*',
+        'HOST': 'mysql',
+        'PORT': 3306,
     }
 }
 
